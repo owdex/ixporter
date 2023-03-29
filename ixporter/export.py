11d8b7ec6bdfaf70a2be2b1882589f58ba521388
alexmshepherd <110127860+alexmshepherd@users.noreply.github.com>
2023-03-29 14:36:57 +0200

    Class-ify

--- ixporter/export.py
+++ ixporter/export.py
@@ -8,25 +8,39 @@ import more_itertools
 from pysolr import Solr
 import jsonlines
 
+class Exporter:
+    def __init__(self, path: Path, entry_split: int = 100, db: Solr|None = None):
+        self.path = path
+        self.entry_split = entry_split
+        if db:
+            self.load_entries(db)
 
-def write_batch(entries: Sequence[dict], directory: Path) -> Path:
-    """Write entries to a single JSONlines batch file."""
-    with StringIO() as buffer:
-        with jsonlines.Writer(buffer) as writer:
-            writer.write(entries)
+    
+    def load_entries(self, db: Solr):
+        self.entries = db.search("*:*").docs
+    
 
-        buffer_value = buffer.getvalue()
-        file_path = directory / f"{md5(buffer_value.encode()).hexdigest()}.jsonl"
-        with file_path.open('w+') as file:
-            file.write(buffer_value)
+    def _write_batch(self, batch: list) -> Path:
+        """Write entries to a single JSONlines batch file."""
+        with StringIO() as buffer:
+            with jsonlines.Writer(buffer) as writer:
+                writer.write(batch)
 
-    return file_path
+            buffer_value = buffer.getvalue()
+            file_path = self.path / f"{md5(buffer_value.encode()).hexdigest()}.jsonl"
+            with file_path.open('w+') as file:
+                file.write(buffer_value)
 
-def write_bundle(entries: Sequence[dict], directory: Path, split: int = 100) -> Path:
-    """Split entries into batches, and write a bundle of those files with a manifest."""
-    for batch in more_itertools.batched(entries, split):
-        write_batch(batch, directory)
+        return file_path
 
-def record_to_entries(record: Path) -> list[dict]:
-    with jsonlines.Reader(record) as reader:
-        return [entry for entry in reader]
+
+    def write_bundle(self):
+        """Split entries into batches, and write a bundle of those files with a manifest."""
+        for batch in more_itertools.batched(self.entries, self.entry_split):
+            self._write_batch(batch)
+
+ex = Exporter(
+    Path("."),
+    db = Solr("http://localhost:8983/solr/gettingstarted")
+)
+ex.write_bundle()
