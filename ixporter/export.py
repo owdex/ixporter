14f6521b379c8f2a94c3478cde6fe2a023af1f00
4censord <mail@4censord.de>
2023-04-04 21:31:22 +0200

    Fix the exporter
    It now
    * exports all records
    * Uses jsonlines, and no longer generates one long list of records
    * compresses them with gzip
    * Splits them up into files with 10000 records each
    
    TODO: Check if 10000 is a good value.

--- ixporter/export.py
+++ ixporter/export.py
@@ -1,47 +1,93 @@
-from io import StringIO
-from hashlib import md5
+import gzip
+import hashlib
+import json
+import os
+import random
+import shutil
+import string
+import tempfile
+
 from pathlib import Path
-from typing import Sequence
-from more_itertools import batched
+from more_itertools import chunked
 
 from pysolr import Solr
 import jsonlines
+
 
 class Exporter:
     def __init__(self, path: Path, db: Solr, entry_split: int = 100):
         self.version = "0.1"
         self.path = path
         self.path.mkdir(parents=True, exist_ok=True)
+        self.tmpdir = tempfile.TemporaryDirectory(prefix="ixporter_")
         self.entry_split = entry_split
-        self.entries = db.search("*:*").docs
+        self.entries = db.search("*:*", sort="id ASC", cursorMark="*")
 
-    
-    def _get_bundle_checksum(self):
-        checksum = md5()
-        for batch in self.path.iterdir():
-            checksum.update(batch.name.encode())
-        return checksum
+    def _get_bundle_checksums(self, filename):
+        sha256 = hashlib.sha256()
 
+        with open(filename, "rb") as file:
+            while True:
+                data = file.read(sha256.block_size)
+                if not data:
+                    break
+                sha256.update(data)
+
+        return {"sha256": sha256.hexdigest()}
+
+    def _id_generator(self):
+        """creates 8 long random sequences of ascii chars. To be used for temporary files"""
+        return "".join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+    def _create_bundle(self, records):
+        """Takes a list of records and writes out a bundle file
+        Returns information about the bundle file, for inclusion in the manifest
+        """
+
+        tmpfilepath = Path(f"{self.tmpdir.name}/tmp_{self._id_generator()}.jsonl.gz")
+        tmpfilepath.touch()
+        # Todo: consider different compression algorithms
+        with gzip.open(tmpfilepath, mode="wb") as file:
+            with jsonlines.Writer(file) as writer:
+                for record in records:
+                    writer.write(record)
+
+        digests = self._get_bundle_checksums(tmpfilepath)
+
+        folder = self.path / digests["sha256"][0:2]
+        Path(folder).mkdir(exist_ok=True)
+
+        filepath = folder / f"{digests['sha256'][2:10]}.jsonl.gz"
+        shutil.move(tmpfilepath, filepath)
+
+        return {
+            "path": filepath.name,
+            "checksums": digests,
+            "filesize": os.stat(filepath).st_size,
+            "fragment_version": 0.1,
+            "records": len(records),
+            "compression": "gzip",
+        }
 
     def write_bundle(self):
         """Split entries into batches, and write a bundle of those files with a manifest."""
-        for batch in batched(self.entries, self.entry_split):
-            with StringIO() as buffer:
-                with jsonlines.Writer(buffer) as writer:
-                    writer.write(batch)
 
-                buffer_value = buffer.getvalue()
-                file_path = self.path / f"{md5(buffer_value.encode()).hexdigest()}.jsonl"
-                with file_path.open('w+') as file:
-                    file.write(buffer_value)
+        bundel_files = []
+        for batch in chunked(self.entries, 10000):
+            bundel = self._create_bundle(batch)
+            bundel_files.append(bundel)
+            print(bundel)
 
-        manifest_path = self.path / "manifest.json"
-        with jsonlines.Writer(manifest_path.open(mode = "w+")) as writer:
-            writer.write({
-            "version": self.version,
-            "records": len(self.entries),
-            "checksums": {
-                "md5": self._get_bundle_checksum().hexdigest(),
-            },
-            "compression": "None"
-        })
+        total_records: int = 0
+        for bundel in bundel_files:
+            total_records += bundel["records"]
+
+        manifest_path = f"{self.path}/manifest.json"
+        with open(manifest_path, mode="w") as file:
+            # Don't need jsonlines here
+            file.write(
+                json.dumps({
+                    "files": bundel_files,
+                    "index_version": 1,
+                    "records": total_records,
+                }))
